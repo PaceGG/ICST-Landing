ab0ece895698a2f1da9dded17cc1e69bc2ae65fb
PaceGG <yura3012004@gmail.com>
2024-04-20 01:42:45 +0500

    about faculties animation from top add

--- script/about_faculties animation.js
+++ script/about_faculties animation.js
@@ -1,4 +1,13 @@
+// Выезд сбоку
 var childElements = document.querySelectorAll(".about_faculties__text");
+childElements.forEach(function (childElement) {
+  var childHeight = childElement.clientHeight;
+  var parentElement = childElement.parentElement;
+  parentElement.style.height = childHeight + "px";
+});
+
+// Выезд сверху
+var childElements = document.querySelectorAll(".about_faculties__text_box");
 childElements.forEach(function (childElement) {
   var childHeight = childElement.clientHeight;
   var parentElement = childElement.parentElement;
