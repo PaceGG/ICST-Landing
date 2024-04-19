517d02f818e43e6c31f3e885fc5bcc3b0073f1cd
PaceGG <yura3012004@gmail.com>
2024-04-20 02:10:07 +0500

    about faculties scroll animation

--- script/about_faculties animation.js
+++ script/about_faculties animation.js
@@ -13,3 +13,32 @@ childElements.forEach(function (childElement) {
   var parentElement = childElement.parentElement;
   parentElement.style.height = childHeight + "px";
 });
+
+// Анимация при скролле
+window.addEventListener("scroll", function () {
+  var screenPosition = window.innerHeight;
+
+  var block1 = document.getElementById("about_faculties__top_animation_area_1");
+  var block1Position = block1.getBoundingClientRect().top;
+  if (block1Position < screenPosition) {
+    block1.classList.add("about_faculties__animation_apply");
+  }
+
+  var block2 = document.getElementById("about_faculties__top_animation_area_2");
+  var block2Position = block2.getBoundingClientRect().top;
+  if (block2Position < screenPosition) {
+    block2.classList.add("about_faculties__animation_apply");
+  }
+
+  var block3 = document.getElementById("about_faculties__top_animation_area_3");
+  var block3Position = block3.getBoundingClientRect().top;
+  if (block3Position < screenPosition) {
+    block3.classList.add("about_faculties__animation_apply");
+  }
+
+  var block4 = document.getElementById("about_faculties__top_animation_area_4");
+  var block4Position = block4.getBoundingClientRect().top;
+  if (block4Position < screenPosition) {
+    block4.classList.add("about_faculties__animation_apply");
+  }
+});
