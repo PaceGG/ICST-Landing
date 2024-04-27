9ed70c7863fc60bfa612aaf22c217c638997ef36
Alexnyshka <alex135poluha@gmail.com>
2024-04-27 22:16:58 +0500

    переделал анимацию в о факультетах

--- script/about_faculties animation.js
+++ script/about_faculties animation.js
@@ -21,24 +21,32 @@ window.addEventListener("scroll", function () {
   var block1 = document.getElementById("about_faculties__top_animation_area_1");
   var block1Position = block1.getBoundingClientRect().top;
   if (block1Position < screenPosition) {
+    var h3_1 = document.getElementById("about_faculties__h3_1");
     block1.classList.add("about_faculties__animation_apply");
+    h3_1.classList.add("about_faculties__animation_h3_left");
   }
 
   var block2 = document.getElementById("about_faculties__top_animation_area_2");
   var block2Position = block2.getBoundingClientRect().top;
   if (block2Position < screenPosition) {
+    var h3_2 = document.getElementById("about_faculties__h3_2");
     block2.classList.add("about_faculties__animation_apply");
+    h3_2.classList.add("about_faculties__animation_h3_right");
   }
 
   var block3 = document.getElementById("about_faculties__top_animation_area_3");
   var block3Position = block3.getBoundingClientRect().top;
   if (block3Position < screenPosition) {
+    var h3_3 = document.getElementById("about_faculties__h3_3");
     block3.classList.add("about_faculties__animation_apply");
+    h3_3.classList.add("about_faculties__animation_h3_left");
   }
-
+  
   var block4 = document.getElementById("about_faculties__top_animation_area_4");
   var block4Position = block4.getBoundingClientRect().top;
   if (block4Position < screenPosition) {
+    var h3_4 = document.getElementById("about_faculties__h3_4");
     block4.classList.add("about_faculties__animation_apply");
+    h3_4.classList.add("about_faculties__animation_h3_right");
   }
 });
