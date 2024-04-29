76c1d813c8d506c798dccb01b320565da954314a
Alexnyshka <alex135poluha@gmail.com>
2024-04-29 20:26:37 +0500

    зациклил анимацию в слайдере

--- script/about.js
+++ script/about.js
@@ -1,61 +1,105 @@
+        const slides = document.querySelectorAll(".slide");
+        const slideHeight = slides[0].offsetHeight;
+        const marginValue = parseInt(window.getComputedStyle(slides[0]).marginTop);
+        var slideHeight_main = slideHeight + marginValue
+        const slidesArrPosition = [];
+        if (slides.length % 2 === 0)
+        {
+          var slidesHeight_main = -(slideHeight_main * (slides.length - 2)) / 2;
+        }else{
+          var slidesHeight_main = -(slideHeight_main * (slides.length - 1)) / 2;
+        }
+        for (let i = 0; i < slides.length; i++) 
+        {
+          slides[i].style.transform = `translateY(${slidesHeight_main}px)`;
+          slidesArrPosition.push(slidesHeight_main);
+          slidesHeight_main = slidesHeight_main + slideHeight_main;
+          //console.log(1);
+        }
+
+
 document.addEventListener("DOMContentLoaded", function() 
 {
     const arrowUp = document.getElementById("arrow_up");
     const arrowDown = document.getElementById("arrow_down");
-    const slides = document.querySelectorAll(".slide");
-    var slideUp = true;
-    var slideDown = true;
-    let currentSlide = 0;
   
-    arrowOpacity(); 
+   
   
     arrowUp.addEventListener("click", function() {
-      if (currentSlide > 0) 
-      {
-        currentSlide--;
-        sliderPosition();
-      }
-      arrowOpacity();
+        sliderPosition(0);
     });
   
     arrowDown.addEventListener("click",function() {
-      if (currentSlide < slides.length- 2) 
-      {
-        currentSlide++;
-        sliderPosition();
-      }
-      arrowOpacity(); 
+        sliderPosition(1);
     });
   
-    function sliderPosition() {
-        const slideHeight = slides[0].offsetHeight;
-        const marginValue = parseInt(window.getComputedStyle(slides[0]).marginTop);
-        const displacement = -currentSlide * slideHeight - (marginValue + 1) * currentSlide;
-        console.log(currentSlide);
-        for (let i = 0; i < slides.length; i++) 
+    function sliderPosition(direction) {
+        const slider = document.querySelector(".slider");
+        if(direction === 0)
         {
-          slides[i].style.transform = `translateY(${displacement}px)`;
+          // нижний вверх
+          slider.prepend(slider.lastElementChild);
+          const slides = document.querySelectorAll(".slide");
+          //console.log(slidesArrPosition);
+          for (let i = 0; i < slides.length ; i++) 
+          {
+            //console.log(slidesArrPosition[i] );
+            const animation = slides[i].animate(
+              [
+                 { transform: `translateY(${slidesArrPosition[i] - slideHeight_main}px)` },
+                { transform: `translateY(${slidesArrPosition[i]}px)`, offset: 1 } 
+              ],
+              {
+                duration: 500,
+                easing: 'linear'
+              }
+            );
+            animation.onfinish = () => {
+              slides[i].style.transform = `translateY(${slidesArrPosition[i]}px)`;
+            };
+          }
+        }else if (direction === 1)
+        {
+          // верхний вниз 
+          slider.append(slider.firstElementChild);
+
+
+          const slides = document.querySelectorAll(".slide");
+          //console.log(slidesArrPosition);
+          for (let i = 0; i < slides.length ; i++) 
+          {
+            //console.log(slidesArrPosition[i] );
+            const animation = slides[i].animate(
+              [
+                 { transform: `translateY(${slidesArrPosition[i] + slideHeight_main}px)` },
+                 { transform: `translateY(${slidesArrPosition[i]}px)`, offset: 1 } 
+              ],
+              {
+                duration: 500,
+                easing: 'linear'
+              }
+            );
+            animation.onfinish = () => {
+              slides[i].style.transform = `translateY(${slidesArrPosition[i]}px)`;
+            };
+          }
         }
+
+        // const slideHeight = slides[0].offsetHeight;
+        // const marginValue = parseInt(window.getComputedStyle(slides[0]).marginTop);
+        // const displacement = -currentSlide * slideHeight - (marginValue + 1) * currentSlide;
+        // console.log(currentSlide);
+         //for (let i = 0; i < slides.length; i++) 
+        // {
+        //   slides[i].style.transform = `translateY(${displacement}px)`;
+        // }
     }
-  
-    function arrowOpacity() {
-      if (currentSlide === 0) 
-      {
-        arrowUp.classList.add("disabled");
-        slideUp = false;
-      } else {
-        arrowUp.classList.remove("disabled");
-        slideUp = true;
-      }
-  
-      if (currentSlide === slides.length - 2) 
-      {
-        arrowDown.classList.add("disabled");
-        slideDown = false;
-      } else {
-        arrowDown.classList.remove("disabled");
-        slideDown = true;
-      }
-    }
-  });
+
+});
+
+
+
+
+
+
   
