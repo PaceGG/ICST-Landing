80171fa9c8c2ec0d5feb898ff05c61fa4a61cc81
Alexnyshka <alex135poluha@gmail.com>
2024-05-07 00:55:57 +0500

    добавил отзывы и сделал в мобильной версии партнеров слайдером

--- script/partners.js
+++ script/partners.js
@@ -1,163 +1,195 @@
 
-// window.addEventListener('load', function() {
-//     const partnersContent = document.querySelector('.partners__content');
-//     const partners = partnersContent.querySelectorAll('.partner');
-//     const partners_ = partnersContent.querySelectorAll('.partner_');
-//     const duration = 10000; // Продолжительность анимации в миллисекундах
-//     const delay = 200; // Задержка между анимациями элементов в миллисекундах
-//     const containerWidth = partnersContent.offsetWidth;
-//     const partnerWidth = partners[0].offsetWidth; // Ширина одного логотипа
-//     const marginLeftPercentage = 0.10; // 20% ширины контейнера для margin-left
-//     const marginLeft = containerWidth * marginLeftPercentage; // Отступ слева для каждого логотипа
-  
-//     partners.forEach((partner, index) => {
-//       const offset = index;
-//       const lastPartnerOffset = (partners_.length + partners.length- 2) * (partnerWidth + marginLeft);
-//       const animation = partner.animate(
-//         [
-//           { transform: 'translateX(0)', opacity: 1 },
-//           { transform: `translateX(${-containerWidth*2}px)`, opacity: 1 },
-//           { transform: `translateX(${containerWidth*2}px)`, opacity: 0, easing: 'step-end' },
-//           { transform: 'translateX(0)', opacity: 1 }
-//         ],
-//         {
-//           duration: duration,
-//           delay: offset,
-//           iterations: Infinity,
-//           easing: 'linear'
-//         }
-//       );
-//     });
-// });
-// window.addEventListener('load', function() {
-//     const partnersContent = document.querySelector('.partners__content');
-//     const partners_ = partnersContent.querySelectorAll('.partner_');
-//     const partners = partnersContent.querySelectorAll('.partner');
-//     const duration = 15000; // Продолжительность анимации в миллисекундах
-//     const delay = 200; // Задержка между анимациями элементов в миллисекундах
-//     const containerWidth = partnersContent.offsetWidth;
-//     const partnerWidth = partners_[0].offsetWidth; // Ширина одного логотипа
-//     const marginLeftPercentage = 0.10; // 20% ширины контейнера для margin-left
-//     const marginLeft = containerWidth * marginLeftPercentage; // Отступ слева для каждого логотипа
-  
-//     partners_.forEach((partner, index) => {
-//       const offset = index;
-//       const lastPartnerOffset = (partners_.length + partners.length- 2) * (partnerWidth + marginLeft);
-//       const animation = partner.animate(
-//         [
-//           { transform: 'translateX(0)', opacity: 1 },
-//           { transform: `translateX(${-containerWidth*4}px)`, opacity: 1 },
-//           { transform: 'translateX(0)', opacity: 0, easing: 'step-end' }
-//         ],
-//         {
-//           duration: duration,
-//           delay: offset,
-//           iterations: Infinity,
-//           easing: 'linear'
-//         }
-//       );
-//     });
-// });
-
-
-// var k = 0;
-// const container = document.querySelector('.partners__content_wrap');
-// const content = document.querySelector('.partners__content');
-// const partners = Array.from(document.querySelectorAll('.partner'));
-// let currentPosition = 0;
-// const speed = 10;
-// let isFirstPartnerCloned = false; // Флаг для отслеживания клонирования первого элемента
-
-// function movePartners() {
-//   currentPosition += speed;
-//   partners.forEach(partner => {
-//     const rect = partner.getBoundingClientRect();
-//     // Перемещаем элементы влево
-//     const translation = 300 * (k + 1) - currentPosition;
-//     partner.style.transform = `translateX(-${translation}px)`;
-
-//   });
-
-//   // Проверьте, если первый элемент вышел за пределы экрана и не был еще клонирован
-//   const firstPartnerRect = partners[k].getBoundingClientRect();
-//   if (firstPartnerRect.right < -500) {
-//     const firstPartnerClone = partners[k].cloneNode(true);
-//     content.appendChild(firstPartnerClone);
-//     partners.push(firstPartnerClone);
-//     content.removeChild(partners[k]);
-//     k++;
-//     if (k >= partners.length)
-//     { 
-//         k = 0;
-//     } 
-//   }
-//   // Повторите анимацию
-//   requestAnimationFrame(movePartners);
-// }
-
-// // Начните анимацию
-// movePartners();
-const content1 = document.querySelector('#partners__content1');
-const content2 = document.querySelector('#partners__content2');
-content1.addEventListener('mouseenter', pauseAnimation);
-content1.addEventListener('mouseleave', resumeAnimation);
-content2.addEventListener('mouseenter', pauseAnimation);
-content2.addEventListener('mouseleave', resumeAnimation);
-
-function pauseAnimation() 
+if (!isMobile)
 {
-    content1.classList.add('paused');
-    content2.classList.add('paused');
+    document.querySelector('.slider_box_partners').style.display = 'none';
+    const content1 = document.querySelector('#partners__content1');
+    const content2 = document.querySelector('#partners__content2');
+    content1.addEventListener('mouseenter', pauseAnimation);
+    content1.addEventListener('mouseleave', resumeAnimation);
+    content2.addEventListener('mouseenter', pauseAnimation);
+    content2.addEventListener('mouseleave', resumeAnimation);
+
+    function pauseAnimation() 
+    {
+        content1.classList.add('paused');
+        content2.classList.add('paused');
+    }
+
+    function resumeAnimation() 
+    {
+        content1.classList.remove('paused');
+        content2.classList.remove('paused');
+    }
 }
 
-function resumeAnimation() 
+if (isMobile)
 {
-    content1.classList.remove('paused');
-    content2.classList.remove('paused');
+    
+    document.getElementById('partners__content1').style.display = 'none';
+    document.getElementById('partners__content2').style.display = 'none';
+    const slides = document.querySelectorAll(".partners_slide");
+    const slideWidth = slides[0].offsetWidth;
+    const marginValue = parseInt(window.getComputedStyle(slides[0]).marginLeft);
+    //  console.log(marginValue);
+    var slideWidth_main = slideWidth + (marginValue);
+    const slidesArrPosition = [];
+    if (slides.length % 2 === 0)
+    {
+      var slidesWidth_main = -(slideWidth_main * (slides.length - 2)) / 2;
+    }else{
+      var slidesWidth_main = -(slideWidth_main * (slides.length - 1)) / 2;
+    }
+    for (let i = 0; i < slides.length; i++) 
+    {
+      slides[i].style.transform = `translateX(${slidesWidth_main}px)`;
+      slidesArrPosition.push(slidesWidth_main);
+      slidesWidth_main = slidesWidth_main + slideWidth_main;
+      //console.log(1);
+    }
+  
+  
+    const slaiderList_mobal = document.querySelector('.partners_slider');
+    let startX, currentX, offsetX, direction, offsetX_prev = 0, diff = 0;
+    const slaiders_mobal = Array.from(slaiderList_mobal.children);
+  
+    function TouchStart(e) {
+      startX = e.touches[0].clientX;
+      offsetX = slaiderList_mobal.scrollLeft;
+    }
+  
+    function TouchMove(e, slides) {
+      currentX = e.touches[0].clientX;
+      diff = currentX - startX;
+      //slaiderList_mobal.scrollLeft = offsetX - diff;
+      direction = (offsetX - diff < offsetX_prev )? 1 : 0;
+      offsetX_prev = offsetX - diff;
+      if (direction === 1){
+        for (let i = 0; i < slides.length ; i++) 
+          {
+            slides[i].style.transform = `translateX(${slidesArrPosition[i] + diff}px)`;
+          }
+      }else{
+        for (let i = 0; i < slides.length ; i++) 
+          {
+            slides[i].style.transform = `translateX(${slidesArrPosition[i] + diff}px)`;
+          }
+      }
+      //console.log(slides);
+    }
+    function TouchEnd() { 
+      // вправо  
+      if (direction === 1 && diff >= 70) 
+      {
+        slaiderList_mobal.prepend(slaiderList_mobal.lastElementChild);
+        const slides = document.querySelectorAll(".partners_slide");
+        //console.log(slidesArrPosition);
+        
+        for (let i = 0; i < slides.length ; i++) 
+        {
+          const animation = slides[i].animate(
+            [
+              { transform: `translateX(${slidesArrPosition[i] - slideWidth_main + (diff)}px)` },
+              { transform: `translateX(${slidesArrPosition[i]}px)`, offset: 1 } 
+            ],
+            {
+              duration: 300,
+              easing: 'linear'
+            }
+          );
+          animation.onfinish = () => {
+            slides[i].style.transform = `translateX(${slidesArrPosition[i]}px)`;
+          };
+          //slidesArrPosition[i] = slidesArrPosition[i] + diff;
+        }
+      }else if(direction === 0 && diff <= -70){// влево  
+        //slaiderList_mobal.append(slaiderList_mobal.firstElementChild);
+        slaiderList_mobal.append(slaiderList_mobal.firstElementChild);
+        const slides = document.querySelectorAll(".partners_slide");
+        //console.log(1);
+        //console.log(diff_main);
+    
+        for (let i = 0; i < slides.length ; i++) 
+        {
+          const animation = slides[i].animate(
+            [
+              { transform: `translateX(${slidesArrPosition[i] + slideWidth_main + diff}px)` },
+              { transform: `translateX(${slidesArrPosition[i]}px)`, offset: 1 } 
+            ],
+            {
+              duration: 300,
+              easing: 'linear'
+            }
+          );
+          animation.onfinish = () => {
+            slides[i].style.transform = `translateX(${slidesArrPosition[i]}px)`;
+          };
+         // console.log(offsetX );
+        
+          //slidesArrPosition[i] = slidesArrPosition[i] - diff ;
+         // console.log(slidesArrPosition[i] );
+        }
+      }else if (direction === 1 && diff < 70) 
+        {
+          const slides = document.querySelectorAll(".partners_slide");
+          //console.log(slidesArrPosition);
+          //console.log(diff_main);
+          //console.log(diff);
+          //console.log(1);
+          for (let i = 0; i < slides.length ; i++) 
+          {
+            const animation = slides[i].animate(
+              [
+                { transform: `translateX(${slidesArrPosition[i] + (diff)}px)` },
+                { transform: `translateX(${slidesArrPosition[i]}px)`, offset: 1 } 
+              ],
+              {
+                duration: 300,
+                easing: 'linear'
+              }
+            );
+            animation.onfinish = () => {
+              slides[i].style.transform = `translateX(${slidesArrPosition[i]}px)`;
+            };
+            //slidesArrPosition[i] = slidesArrPosition[i] + diff;
+          }
+        }else if(direction === 0 && diff > -70){// влево  
+          //slaiderList_mobal.append(slaiderList_mobal.firstElementChild);
+          const slides = document.querySelectorAll(".partners_slide");
+          //console.log(1);
+          //console.log(diff_main);
+          //console.log(diff);
+          //console.log(2);
+          for (let i = 0; i < slides.length ; i++) 
+          {
+            const animation = slides[i].animate(
+              [
+                { transform: `translateX(${slidesArrPosition[i] + diff}px)` },
+                { transform: `translateX(${slidesArrPosition[i]}px)`, offset: 1 } 
+              ],
+              {
+                duration: 300,
+                easing: 'linear'
+              }
+            );
+            animation.onfinish = () => {
+              slides[i].style.transform = `translateX(${slidesArrPosition[i]}px)`;
+            };
+           // console.log(offsetX );
+          
+            //slidesArrPosition[i] = slidesArrPosition[i] - diff ;
+           // console.log(slidesArrPosition[i] );
+          }
+        }
+        diff = 0;
+    }
+  
+  
+    slaiderList_mobal.addEventListener('touchstart', TouchStart);
+    slaiderList_mobal.addEventListener('touchmove', (e) => TouchMove(e, document.querySelectorAll(".partners_slide")));
+    slaiderList_mobal.addEventListener('touchend', TouchEnd);
+
 }
 
 
 
 
-// const partnersContent1 = document.getElementById('partners__content1');
-// const partnersContent2 = document.getElementById('partners__content2');
-
-// let isDragging = false;
-// let currentX;
-// let initialX;
-// let xOffset = 0;
-
-// function dragStart(e) {
-//     initialX = e.clientX - xOffset;
-//     isDragging = true;
-//     partnersContent1.classList.remove('paused');
-//     partnersContent2.classList.remove('paused');
-// }
-
-// function dragEnd() {
-//     isDragging = false;
-//     partnersContent1.classList.add('paused');
-//     partnersContent2.classList.add('paused');
-// }
-
-// function drag(e) {
-//     if (isDragging) {
-//         e.preventDefault();
-
-//         currentX = e.clientX - initialX;
-//         xOffset = currentX;
-
-//         setTranslate(currentX, partnersContent1, partnersContent2);
-//     }
-// }
-
-// function setTranslate(xPos, el1, el2) {
-//     el1.style.left = `${xPos}px`;
-//     el2.style.left = `${xPos + el1.offsetWidth}px`;
-// }
-
-// partnersContent1.addEventListener('mousedown', dragStart);
-// partnersContent2.addEventListener('mousedown', dragStart);
-
-// document.addEventListener('mouseup', dragEnd);
-// document.addEventListener('mousemove', drag);
