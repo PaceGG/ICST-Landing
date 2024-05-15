aeda32c642f43bcbf8e33f7ca5f9a8409f999fd6
Alexnyshka <alex135poluha@gmail.com>
2024-05-15 21:34:05 +0500

    рулетка

--- script/roulette.js
+++ script/roulette.js
@@ -5,6 +5,7 @@ const string_arr1 = Array.from(rouletteArr1.querySelectorAll('p'));
 const min = 1;
 const max = string_arr1.length - 1;
 const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+//const randomNumber = 15;
 var rouletteFlag = true;
 
 rouletteButton.addEventListener('click', () => {
@@ -12,7 +13,8 @@ rouletteButton.addEventListener('click', () => {
         rouletteArr1.removeChild(rouletteArr1.lastElementChild);
         const rouletteArr = document.querySelector('.roulette_arr');
         const string_arr = Array.from(rouletteArr.querySelectorAll('p'));
-        const rowHeight = string_arr[0].offsetHeight + parseFloat(window.getComputedStyle(string_arr[0]).marginTop)+ 0.6;
+        const margin_p = parseFloat(window.getComputedStyle(string_arr[0]).marginTop);
+        const rowHeight = string_arr[0].offsetHeight + margin_p;
         const clonElements = [];
         string_arr.forEach((p, index) => {
         const clonstring = p.cloneNode(true);
@@ -20,7 +22,7 @@ rouletteButton.addEventListener('click', () => {
         });
         rouletteArr.innerHTML = clonElements.join('') + rouletteArr.innerHTML;
 
-        const offset = (randomNumber * rowHeight) + ((max - 1) * rowHeight + 5);
+        const offset = (randomNumber * rowHeight) + ((max - 1) * rowHeight + 5) - margin_p/4;
         rouletteArr.style.transition = 'transform 3s cubic-bezier(0.2, 0.8, 0.58, 1)';
         rouletteArr.style.transform = `translateY(${offset}px)`;
         rouletteFlag = false;
